58b1d49ae5e9ea5b49eeca28f9d5a719463d9cd9
bubuStyle <bubustyle4@gmail.com>
2017-02-16 16:20:09 +0100

    Sauvegarde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// 1) Importer la class du service :
+import { ServicesService } from './services/services.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html'
+  templateUrl: './app.component.html',
+  // 2) Ajouter le service dans le tableau des providers :
+  providers: [ServicesService]
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  // Créer une variable pour récup les données du service :
+  myList: any[];
+  // 3) Définir une variable pour utiliser le service :
+  constructor(
+    private servicesService: ServicesService
+  ) {}
+
+  // Créer une fonction pour récupérer la liste des étudiants :
+  getStudentFromService(){
+    this.servicesService.showList().then( data => this.myList = data );
+  }
+
+  ngOnInit() {}
 }
--- src/app/services/services.service.ts
+++ src/app/services/services.service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
 
+// Importer le système de promesse :
+import 'rxjs/add/operator/toPromise';
+
+// Créer et exporter une collection de données :
+export const LIST = [
+  {
+    category: '',
+    nom: '',
+    comment: '',
+    option: ''
+  }
+];
+
 @Injectable()
 export class ServicesService {
 
   constructor() { }
+
+  // Créer une fonction pour envoyer la liste des étudiants :
+  showList(): Promise<any[]>{
+
+    // Renvoyer le contenu de LIST :
+    return Promise.resolve(LIST);
+
+  }
 
 }
