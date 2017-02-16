6828b1fc7b96753b03183dcb84a2c08fe7220e50
bubuStyle <bubustyle4@gmail.com>
2017-02-16 13:08:24 +0100

    Ajout du CSS

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './directives/header/header.component';
+import { FooterComponent } from './directives/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
